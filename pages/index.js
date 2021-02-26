3dc5d008222d4fe63d6c6df63f8fcdd3ddaad617
doberman7 <ivanrubioangeles@gmail.com>
2021-02-26 13:50:27 -0600

    1st commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import "antd/dist/antd.css";
+
+import Layout from "../components/layout";
+
+const { Header, Content, Footer } = Layout;
+
+export default function Home() {
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  );
+}
