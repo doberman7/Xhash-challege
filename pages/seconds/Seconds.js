214c77c6b1efea5d406c83907825fc8a9c90078d
doberman7 <ivanrubioangeles@gmail.com>
2021-03-01 12:57:29 -0600

    form

--- pages/seconds/Seconds.js
+++ pages/seconds/Seconds.js
@@ -3,17 +3,48 @@ import styles from "./seconds.module.css";
 import "antd/dist/antd.css";
 import Link from "next/link";
 import Starting from "./Starting";
+import { Form, Input, Button, Checkbox } from "antd";
 
 const Seconds = () => {
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <>
       <div className={styles.header}>Seconds</div>
-      Every second
-      <Radio defaultChecked={true} />
+      {/* Every second
+      <Radio defaultChecked={true} /> */}
       <Divider />
-      <Starting />
+      <Form>
+        <Form.Item label="Starting" name="Starting" {...tailLayout}>
+          <Starting />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
       <Divider />
-      <br />
+      {/* <br /> */}
       <div className={styles.backToHome}>
         <Link href="/">
           <a>← Back to home</a>
