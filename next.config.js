154a757f45540ab0b62afa5e78e63cb670b1cde9
doberman7 <ivanrubioangeles@gmail.com>
2021-03-21 17:59:14 -0600

    .env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 module.exports = {
   env: {
-    customKey: "valueeeeees",
+    secret: process.env.SECRET,
+    client_id: process.env.CLIENT_ID,
   },
 };
+// console.log(module.exports);
